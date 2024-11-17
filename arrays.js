e19187e045749060bc71d37ed7408ab337da980b
cristhyne007 <160241007+cristhyne007@users.noreply.github.com>
2024-11-16 23:24:34 -0300

    arrays
    day2

---
+++ arrays.js
@@ -0,0 +1,12 @@
+//diminui o número de váriaveis,armazenando todos os objetos em uma variável
+const sesapi=[{
+    nome:"Josiane",
+    endereco:"rua n13 de maio",
+    salário:2000,
+    sala:5,
+    turno:"manha"},{
+    nome:"Thiago",
+    endereço:"rua 7 de setembro",
+    salário:1500,
+    sala:4,
+    turno:"tarde"}]
